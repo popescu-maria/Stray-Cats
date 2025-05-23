15c9542257bf9c03b794b826e82a1078167e5318
Maria <maria.3.poppy@gmail.com>
2025-05-23 11:06:53 +0300

    fixes

--- app/__init__.py
+++ app/__init__.py
@@ -9,9 +9,10 @@ def create_app():
     app = Flask(__name__)
     load_dotenv()
 
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'a_default_secret_key_if_not_set')
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['PORT'] = os.getenv('PORT')
 
     init_extensions(app)
 
